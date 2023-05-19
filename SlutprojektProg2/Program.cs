9b5202703d5f885b63da8eb1db9ac880e42ac9e3
Peamseye <aispotato587@gmail.com>
2023-05-19 23:20:58 +0200

    1241

--- SlutprojektProg2/Program.cs
+++ SlutprojektProg2/Program.cs
@@ -7,10 +7,16 @@ Console.Clear();
 
 Hero h1 = new Hero();
 
+bool playername = true;
+
+while (playername == true) {
 Console.WriteLine("What is the name of your hero?");
 Console.Clear();
 Console.WriteLine("ENTER NAME");
 h1.name = Console.ReadLine();
+
+playername = false;
+}
 
 Console.WriteLine($"Your hero's name is [{h1.name}].");
 
@@ -19,3 +25,4 @@ while (h1.GetHeroAlive() == true)
     Console.Clear();
     h1.HeroStatus();
 }
+Console.ReadLine();
