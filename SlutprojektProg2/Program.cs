eb7635f7eeabf58f3017997eb7dd27a0af08d632
Peamseye <aispotato587@gmail.com>
2023-05-19 23:14:01 +0200

    124124

--- SlutprojektProg2/Program.cs
+++ SlutprojektProg2/Program.cs
@@ -8,6 +8,8 @@ Console.Clear();
 Hero h1 = new Hero();
 
 Console.WriteLine("What is the name of your hero?");
+Console.Clear();
+Console.WriteLine("ENTER NAME");
 h1.name = Console.ReadLine();
 
 Console.WriteLine($"Your hero's name is [{h1.name}].");
